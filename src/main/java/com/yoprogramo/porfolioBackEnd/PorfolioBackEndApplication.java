73eece04f1c990973ab88fda4b908a6332ca6b82
Rod555 <rod570@hotmail.com>
2023-03-28 10:14:36 -0300

    backend inicio

---
+++ src/main/java/com/yoprogramo/porfolioBackEnd/PorfolioBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.yoprogramo.porfolioBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorfolioBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorfolioBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoprogramo/porfolioBackEnd/PorfolioBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoprogramo.porfolioBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorfolioBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
